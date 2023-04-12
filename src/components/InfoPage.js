fd1c6bb2b20909633948461db226ec8a45c51faf
Tomás Carmo <up202007590@g.uporto.pt>
2023-04-12 16:47:29 +0100

    Created a simpler to understand bar chart

--- src/components/InfoPage.js
+++ src/components/InfoPage.js
@@ -1,26 +1,40 @@
-import React, { useState } from 'react';
+import React from 'react';
 import LineButton from './LineButton';
 import BarChart from './BarChart';
+import MyBarChart from './MyBarChart';
 import './InfoPage.css';
 
 function InfoPage(props) {
-
     const data = [
         { x: "Apples", y: 10},
         { x: "Oranges", y: 20},
         { x: "Grapes", y: 8},
         { x: "Bananas", y: 15},
       ];
-    
-  
-    return (
-      
-        <div className="content">
-              <h1 classname= "selectedline" > Linha <LineButton number={props.number}/></h1>
-              <BarChart data={data} width={600} height={400}/>
-              
-        </div>
-        );
+
+    switch(props.number){
+        case 203:
+            return (
+                <div className="content">
+                    <h1 className= "selectedline" > Linha <LineButton number={props.number}/></h1>
+                    <BarChart data={data} width={600} height={400}/>
+                </div>
+            );
+        case 204:
+            return (
+                <div className="content">
+                    <h1 className= "selectedline" > Linha <LineButton number={props.number}/></h1>
+                    <MyBarChart data={data} width={400} height={400}/>
+                </div>
+            );
+        default:
+            return (
+                <div className="content">
+                    <h1 className= "selectedline" > Linha <LineButton number={props.number}/></h1>
+                    <BarChart data={data} width={600} height={400}/>
+                </div>
+            );
+    }
 }
 
 export default InfoPage;
---
+++ src/components/MyBarChart.js
@@ -0,0 +1,69 @@
+import {React, useRef, useEffect} from "react";
+import * as d3 from "d3";
+
+const MyBarChart = ({data, width, height}) => {
+    const chartRef = useRef(null);
+
+    useEffect(() => {
+        const svg = d3.select(chartRef.current);
+        const margin = { top: 20, right: 20, bottom: 40, left: 50 };
+        const innerWidth = width - margin.left - margin.right;
+        const innerHeight = height - margin.top - margin.bottom;
+    
+        const xScale = d3
+          .scaleBand()
+          .domain(data.map(d => d.x))
+          .range([0, innerWidth])
+          .padding(0.1);
+    
+        const yScale = d3
+          .scaleLinear()
+          .domain([0, d3.max(data, d => d.y)])
+          .nice()
+          .range([innerHeight, 0]);
+    
+        const g = svg.append('g').attr('transform', `translate(${margin.left},${margin.top})`);
+    
+        g.append('g')
+          .attr('class', 'axis axis--x')
+          .attr('transform', `translate(0,${innerHeight})`)
+          .call(d3.axisBottom(xScale));
+
+        g.append('text')
+          .attr('class', 'axis-labelx')
+          .attr('text-anchor', 'middle')
+          .attr('x', innerWidth / 2)
+          .attr('y', innerHeight + margin.top + 15)
+          .text('Fruits');
+    
+        g.append('g')
+          .attr('class', 'axis axis--y')
+          .call(d3.axisLeft(yScale));
+
+        g.append('text')
+          .attr('class', 'axis-labely')
+          .attr('text-anchor', 'middle')
+          .attr('x', margin.top - innerHeight / 2)
+          .attr('y', -margin.left / 2)
+          .attr('transform', 'rotate(-90)')
+          .text('Quantity');
+
+        g.selectAll('.bar')
+          .data(data)
+          .enter()
+          .append('rect')
+          .attr('class', 'bar')
+          .attr('x', d => xScale(d.x))
+          .attr('y', d => yScale(d.y))
+          .attr('width', xScale.bandwidth())
+          .attr('height', d => innerHeight - yScale(d.y));
+    }, [data, width, height]);
+
+    return (
+        <svg ref={chartRef} width={width} height={height}>
+            <g className="chart-area"/>
+        </svg>
+    );
+};
+
+export default MyBarChart;
